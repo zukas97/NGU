3d3bf6a9d34e0b9eeb60db0b78879eb26f25d918
zukas97 <zukaslurker97@proton.me>
2024-11-14 18:14:03 -0500

    fixed navbar

--- script.js
+++
@@ -1,8 +0,0 @@
-function loadNavbar() {
-    fetch('./navbar.html')
-	.then(response => response.text())
-	.then(data => {
-	    document.getElementById('navbar').innerHTML = data;
-	});
-}
-window.onload = loadNavbar;
