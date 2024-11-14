3dfe4a317e0db981fb64f3c4d681f48805d76b83
zukas97 <zukaslurker97@proton.me>
2024-11-14 18:25:48 -0500

    fixed navbar

--- navbar.js
+++ navbar.js
@@ -1,9 +1,5 @@
 fetch('navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
-
+    .then(response => response.text())
+    .then(data => {
+	document.getElementById('navbar-placeholder').innerHTML = data;
+    });
