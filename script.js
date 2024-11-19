c845534be15c6d27a45834f2bdf24d47108bae72
zukas97 <zukaslurker97@proton.me>
2024-11-19 21:24:08 +0000

    added back custom site with filtering

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
+var blocked = false;
+function checkurl(inputstring) {
+	switch (true) {
+		case inputstring.includes("porn"):
+			blocked = true;
+			break;
+		case inputstring.includes("XXX"):
+			blocked = true;
+			break;
+		case inputstring.includes("xxx"):
+			blocked = true;
+			break;
+		case inputstring.includes("xnxx"):
+			blocked = true;
+			break;
+		case inputstring.includes("XNXX"):
+			blocked = true;
+			break;
+		case inputstring.includes("onlyfans"):
+			blocked = true;
+			break;
+		case inputstring.includes("xhamster"):
+			blocked = true;
+			break;
+		default:
+			blocked = false;
+			break;
+
+	}
+}
+
+
+
 document.getElementById('setURL').addEventListener('click', function() {
 	var url = document.getElementById('urlInput').value;
 	var iframe = document.getElementById('customSite');
-	iframe.src = url;
+
+	checkurl(url);
+
+	if (blocked) {
+		iframe.src = url;
+
+	}
+
 });
