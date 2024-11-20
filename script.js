a49a29b7f2f8821896967ffc8a1bd531e52a29fe
zukas97 <zukaslurker97@proton.me>
2024-11-20 01:25:21 +0000

    fixed js syntax error

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.getElementById('setURL').addEventListener('click', function() {
 
 });
 
-document,getElementById('reloadButton').addEventListener('click', function() {
+document.getElementById('reloadButton').addEventListener('click', function() {
 	var iframe = document.getElementById('customSite');
 	iframe.contentWindow.location.reload();
 
