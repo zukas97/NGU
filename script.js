5007593d527d744100a8bfa49c320f8ee0b46349
zukas97 <zukaslurker97@proton.me>
2024-12-11 19:49:00 -0500

    revised p*** blocking algorithm

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
 var blocked = false;
 function checkurl(inputstring) {
-	switch (true) {
-		case inputstring.includes("porn"):
-			blocked = true;
-			break;
-		case inputstring.includes("XXX"):
-			blocked = true;
-			break;
-		case inputstring.includes("xxx"):
-			blocked = true;
-			break;
-		case inputstring.includes("xnxx"):
-			blocked = true;
-			break;
-		case inputstring.includes("XNXX"):
-			blocked = true;
-			break;
-		case inputstring.includes("onlyfans"):
-			blocked = true;
-			break;
-		case inputstring.includes("xhamster"):
-			blocked = true;
-			break;
-		case inputstring.includes("xvideos"):
-			blocked = true;
-			break;
-		case inputstring.includes("damplips"):
-			blocked = true;
-			break;
-		default:
-			blocked = false;
-			break;
+	fetch('https://example.com')
+	  .then(response => response.text())
+	  .then(html => {
+	    const parser = new DOMParser();
+	    const doc = parser.parseFromString(html, 'text/html');
+	    const textContent = doc.body.innerText;
 
-	}
+	    const adultKeywords = ['adult', 'porn', 'sex', 'xxx', 'nude'];
+	    const containsAdultContent = adultKeywords.some(keyword => textContent.toLowerCase().includes(keyword));
+
+	    console.log('Contains adult content:', containsAdultContent);
+	  })
+	  .catch(blocked = true);
+
 }
 document.getElementById('setURL').addEventListener('click', function() {
 	var iframe = document.getElementById('customSite');
@@ -53,8 +34,7 @@ document.getElementById('setURL').addEventListener('click', function() {
 
 
 document.getElementById('reloadButton').addEventListener('click', function() {
-	//iframe.src = iframe.src;
 	var iframe = document.getElementById('customSite');
-	iframe.contentWindow.location.reload();
+	iframe.src = iframe.src;
 	console.log("reloaded");
 });
