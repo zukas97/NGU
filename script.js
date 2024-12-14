830459d9b359770ad5d45296bb80ea2c44e560d1
zukas97 <zukaslurker97@proton.me>
2024-12-14 10:22:49 -0500

    fixed navbar hide

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const game = document.getElementById("game");
 const navbar = document.getElementById('nav-placeholder');
-	const url = game.src;
-document.getElementById("fscreen").addEventListener('click', function() {
+const fsbttn = document.getElementById("fscreen")
+const url = game.src;
+fsbttn.addEventListener('click', function() {
 	document.documentElement.requestFullscreen()
 	navbar.style.display = "none";
 	game.style.height = '100vh';
 	game.style.width = '100vw';
-	game.style.zindex = 5;
 	game.src = url;
 	iframe.contentWindow.location.reload();
 });
 
-if (Document.fullscreen) {
-	navbar.style.display = "none";
-	game.src = url;
-}
-else {
-	navbar.style.display = "block";
-	game.src = url;
+document.addEventListener("fullscreenchange", adjustElementSize);
+
+function adjustElementSize() {
+	if (document.fullscreenElement) {
+		navbar.style.display = "none";
+		game.src = url;
+	}
+	else {
+		navbar.style.display = "block";
+		game.style.height = '95vh';
+		game.style.width = '96vw';
+		game.src = url;
+	}
 }
