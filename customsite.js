dabf3fb038217ff6143f230ff5c3e0c6b762f161
zukas97 <zukaslurker97@proton.me>
2024-12-14 10:12:06 -0500

    added fullscreen button

---
+++ customsite.js
@@ -0,0 +1,57 @@
+var blocked = false;
+function checkurl(inputstring) {
+	fetch('inputstring')
+	  .then(response => response.text())
+	  .then(html => {
+	    const parser = new DOMParser();
+	    const doc = parser.parseFromString(html, 'text/html');
+	    const textContent = doc.body.innerText;
+
+	    const adultKeywords = ['adult', 'porn', 'sex', 'xxx', 'nude'];
+	    const containsAdultContent = adultKeywords.some(keyword => textContent.toLowerCase().includes(keyword));
+
+	    console.log('Contains adult content:', containsAdultContent);
+		blocked = true;
+	  })
+	  .catch(blocked = false);
+
+}
+
+
+document.getElementById('urlInput').addEventListener('keydown', function(event) {
+	const iframe = document.getElementById('customSite');
+	const input = document.getElementById('urlInput')
+	const url = input.value;
+	if (event.key === 'Enter') {
+		if (!blocked) {
+			iframe.src = url;
+		}
+		else if (blocked) {
+			console.log("page blocked");
+			alert("page blocked");
+		}
+	
+	}
+});
+
+document.getElementById('setURL').addEventListener('click', function() {
+	const iframe = document.getElementById('customSite');
+	const input = document.getElementById('urlInput')
+	const url = input.value;
+	
+	checkurl(url);
+	if (!blocked) {
+		iframe.src = url;
+	}
+	else if (blocked) {
+		console.log("page blocked");
+		alert("page blocked");
+	}
+
+});
+
+document.getElementById('reloadButton').addEventListener('click', function() {
+	var iframe = document.getElementById('customSite');
+	iframe.src = iframe.src;
+	console.log("reloaded");
+});
--- script.js
+++ script.js
@@ -1,56 +1,21 @@
-var blocked = false;
-function checkurl(inputstring) {
-	fetch('inputstring')
-	  .then(response => response.text())
-	  .then(html => {
-	    const parser = new DOMParser();
-	    const doc = parser.parseFromString(html, 'text/html');
-	    const textContent = doc.body.innerText;
+const game = document.getElementById("game");
+const navbar = document.getElementById('nav-placeholder');
+	const url = game.src;
+document.getElementById("fscreen").addEventListener('click', function() {
+	document.documentElement.requestFullscreen()
+	navbar.style.display = none;
+	game.style.height = '100vh';
+	game.style.width = '100vw';
+	game.style.zindex = 5;
+	game.src = url;
+	iframe.contentWindow.location.reload();
+});
 
-	    const adultKeywords = ['adult', 'porn', 'sex', 'xxx', 'nude'];
-	    const containsAdultContent = adultKeywords.some(keyword => textContent.toLowerCase().includes(keyword));
-
-	    console.log('Contains adult content:', containsAdultContent);
-		blocked = true;
-	  })
-	  .catch(blocked = false);
-
+if (Document.fullscreen) {
+	navbar.style.display = "none";
+	game.src = url;
 }
-
-document.getElementById('urlInput').addEventListener('keydown', function(event) {
-	const iframe = document.getElementById('customSite');
-	const input = document.getElementById('urlInput')
-	const url = input.value;
-	if (event.key === 'Enter') {
-		if (!blocked) {
-			iframe.src = url;
-		}
-		else if (blocked) {
-			console.log("page blocked");
-			alert("page blocked");
-		}
-	
-	}
-});
-
-document.getElementById('setURL').addEventListener('click', function() {
-	const iframe = document.getElementById('customSite');
-	const input = document.getElementById('urlInput')
-	const url = input.value;
-	
-	checkurl(url);
-	if (!blocked) {
-		iframe.src = url;
-	}
-	else if (blocked) {
-		console.log("page blocked");
-		alert("page blocked");
-	}
-
-});
-
-document.getElementById('reloadButton').addEventListener('click', function() {
-	var iframe = document.getElementById('customSite');
-	iframe.src = iframe.src;
-	console.log("reloaded");
-});
+else {
+	navbar.style.display = "block";
+	game.src = url;
+}
