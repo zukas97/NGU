a3ceeecefb8a5f2e46a6c992dfe5b31dde869d89
zukas97 <zukaslurker97@proton.me>
2024-12-16 12:43:01 -0500

    remove borders on fs

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ document.addEventListener("fullscreenchange", adjustElementSize);
 function adjustElementSize() {
 	if (document.fullscreenElement) {
 		navbar.style.display = "none";
+		game.style.border = '0';
+		game.style.borderRadius = '0';
 		fsbttn.style.display = 'none'
 		document.body.overflow = 'hidden';
 		if (game.src === "https://windowslover1234.github.io/games/slope/"){
@@ -29,7 +31,9 @@ function adjustElementSize() {
 		navbar.style.display = "block";
 		fsbttn.style.display = "block";
 		game.style.height = '95vh';
-		game.style.width = '96vw';
+		game.style.width = '99vw';
+		game.style.border = '1px';
+		game.style.borderRadius = '8px';
 		document.body.overflow = 'scroll';
 		//game.src = url;
 		if (game.src == "https://windowslover1234.github.io/games/slope/"){
